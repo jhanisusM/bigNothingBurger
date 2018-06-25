adeeb921bb67916b785c8919b70416e8ee5fc7ec
jhanisusM <jhanisus@gmail.com>
2018-06-24 23:41:56 -0700

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,51 @@
-// Set up MySQL connection.
-var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "cali",
-  database: "hamburger"
-});
-
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
-// Export connection for our ORM to use.
+var Sequelize = require("sequelize");
+var mysql = require('mysql');
+var connection;
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({
+    host:"jsftj8ez0cevjz8v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user:"feyehybj0ix45suv",
+    password:"ylisz7w3mes94obh",
+    database: "upmtz8jgr4iub4ut"
+  });
+};
+connection.connect();
 module.exports = connection;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Set up MySQL connection.
+// var mysql = require("mysql");
+
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "cali",
+//   database: "hamburger"
+// });
+
+// // Make connection.
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+
+// // Export connection for our ORM to use.
+// module.exports = connection;
